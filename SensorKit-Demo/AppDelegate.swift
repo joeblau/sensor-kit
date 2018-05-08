2c956ff492e0e7d18ff4bcc158c7fb2ae4fae71f
Joe Blau <josephblau@gmail.com>
2018-05-08 18:40:31 -0400

    Added SensorKit Demo app

---
+++ SensorKit-Demo/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  SensorKit-Demo
+//
+//  Created by Joe Blau on 5/8/18.
+//  Copyright © 2018 Bodlog. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+    
+}
+
